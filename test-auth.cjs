c4a1f00f467f17a4f6d74088ec0300f61f4f789f
PrinDDI <diletta.abbonato@unimib.it>
2026-08-19 21:56:47 +0200

    Remove test files containing hardcoded Overleaf token

--- test-auth.cjs
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-
-https.get('https://git.overleaf.com/60a1234567890abcdef12345/info/refs?service=git-upload-pack', (res) => {
-  console.log("Status:", res.statusCode);
-  console.log("Headers:", res.headers);
-  res.on('data', d => process.stdout.write(d));
-});
